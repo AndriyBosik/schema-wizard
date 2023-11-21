4368a0bc432193694d962786ccb3accd4f410713
Nikolay <nikolyand@gmail.com>
2023-11-21 10:02:16 +0200

    SW-2 | Implement migration analyzer (moved migration table name and its column names to constants)

---
+++ src/main/java/com/example/dao/Constants.java
@@ -0,0 +1,12 @@
+package com.example.dao;
+
+public final class Constants {
+    private Constants() {
+    }
+
+    public static final String MIGRATION_TABLE_NAME = "migration_history";
+    public static final String ID = "id";
+    public static final String VERSION = "version";
+    public static final String DESCRIPTION = "description";
+    public static final String APPLIED_ON = "applied_on";
+}
--- src/main/java/com/example/dao/impl/PostgresHistoryTableQueryFactory.java
+++ src/main/java/com/example/dao/impl/PostgresHistoryTableQueryFactory.java
@@ -2,29 +2,38 @@ package com.example.dao.impl;
 
 import com.example.dao.HistoryTableQueryFactory;
 
+import static com.example.dao.Constants.APPLIED_ON;
+import static com.example.dao.Constants.DESCRIPTION;
+import static com.example.dao.Constants.ID;
+import static com.example.dao.Constants.MIGRATION_TABLE_NAME;
+import static com.example.dao.Constants.VERSION;
+
 public class PostgresHistoryTableQueryFactory implements HistoryTableQueryFactory {
     @Override
     public String getCreateMigrationHistoryTableSql() {
-        return "CREATE TABLE IF NOT EXISTS migration_history ("
-                + "id SERIAL PRIMARY KEY, "
-                + "version INTEGER NOT NULL, "
-                + "description TEXT NOT NULL, "
-                + "applied_on TIMESTAMP NOT NULL DEFAULT now(),"
-                + "checksum INTEGER NOT NULL, "
-                + "success BOOLEAN NOT NULL DEFAULT FALSE"
-                + ");";
+        return String.format("CREATE TABLE IF NOT EXISTS %s ("
+                + "%s SERIAL PRIMARY KEY, "
+                + "%s INTEGER NOT NULL, "
+                + "%s TEXT NOT NULL, "
+                + "%s TIMESTAMP NOT NULL DEFAULT now()"
+                + ");",
+                MIGRATION_TABLE_NAME, ID, VERSION, DESCRIPTION, APPLIED_ON);
     }
 
     @Override
     public String getSelectTableSql() {
-        return "SELECT table_name " +
-                "FROM information_schema.tables " +
-                "WHERE table_type='BASE TABLE' AND table_name='migration_history';";
+        return String.format("SELECT table_name "
+                + "FROM information_schema.tables "
+                + "WHERE table_type='BASE TABLE' "
+                + "AND table_name='%s';",
+                MIGRATION_TABLE_NAME);
     }
 
     @Override
     public String getSelectMigrationsSql() {
-        return "SELECT id, version, description, applied_on " +
-                "FROM migration_history order by version;";
+        return String.format("SELECT %s, %s, %s, %s "
+                        + "FROM %s order by version;",
+                ID, VERSION, DESCRIPTION, APPLIED_ON,
+                MIGRATION_TABLE_NAME);
     }
 }
