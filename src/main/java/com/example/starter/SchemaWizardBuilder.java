0ab15a64c3a9d5f18fa9d32430b25e097df230bd
Andrii Bosyk <andriybosik2002@ukr.net>
2023-11-23 20:19:56 +0200

    SchemaWizard initial fix

--- src/main/java/com/example/starter/SchemaWizardBuilder.java
+++ src/main/java/com/example/starter/SchemaWizardBuilder.java
@@ -3,13 +3,15 @@ package com.example.starter;
 import com.example.di.DiContainer;
 import com.example.metadata.DatabaseProvider;
 import com.example.migration.annotation.Provider;
+import com.example.migration.factory.ColumnTypeFactory;
+import com.example.migration.factory.impl.OracleColumnTypeFactory;
+import com.example.migration.factory.impl.PostgreSqlColumnTypeFactory;
 import com.example.migration.operation.Operation;
 import com.example.migration.operation.resolver.OperationResolver;
-import com.example.migration.operation.resolver.custom.CustomDropTableOperationResolver;
-import com.example.migration.operation.resolver.oracle.OracleAddColumnOperationResolver;
-import com.example.migration.operation.resolver.oracle.OracleCreateTableOperationResolver;
-import com.example.migration.operation.resolver.postgresql.PostgreSqlAddColumnOperationResolver;
-import com.example.migration.operation.resolver.postgresql.PostgreSqlCreateTableOperationResolver;
+import com.example.migration.operation.resolver.oracle.*;
+import com.example.migration.operation.resolver.postgresql.*;
+import com.example.migration.service.OperationService;
+import com.example.migration.service.impl.OperationServiceImpl;
 import com.example.model.ConfigurationProperties;
 import com.example.property.service.ConfigurationPropertiesService;
 import com.example.property.service.PropertyParser;
@@ -21,7 +23,7 @@ import org.yaml.snakeyaml.introspector.PropertyUtils;
 import java.util.AbstractMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Stream;
+import java.util.Set;
 
 public class SchemaWizardBuilder {
     private final DiContainer container;
@@ -44,17 +46,39 @@ public class SchemaWizardBuilder {
         ConfigurationPropertiesService propertiesService = container.resolve(ConfigurationPropertiesService.class);
         ConfigurationProperties properties = propertiesService.getProperties();
         container.register(ConfigurationProperties.class, properties);
+        container.register(OperationService.class, OperationServiceImpl.class);
+        container.register(ColumnTypeFactory.class, PostgreSqlColumnTypeFactory.class);
+        container.register(ColumnTypeFactory.class, OracleColumnTypeFactory.class);
 
         DatabaseProvider provider = parseDatabaseProvider(properties.getConnectionUrl());
 
-        Stream.of(
+        Set.of(
                         PostgreSqlAddColumnOperationResolver.class,
+                        PostgreSqlAddColumnsOperationResolver.class,
+                        PostgreSqlAddForeignKeyOperationResolver.class,
+                        PostgreSqlAddPrimaryKeyOperationResolver.class,
+                        PostgreSqlAddUniqueOperationResolver.class,
                         PostgreSqlCreateTableOperationResolver.class,
+                        PostgreSqlDropColumnOperationResolver.class,
+                        PostgreSqlDropColumnsOperationResolver.class,
+                        PostgreSqlDropForeignKeyOperationResolver.class,
+                        PostgreSqlDropPrimaryKeyOperationResolver.class,
+                        PostgreSqlDropTableOperationResolver.class,
+                        PostgreSqlDropUniqueOperationResolver.class,
                         OracleAddColumnOperationResolver.class,
+                        OracleAddColumnsOperationResolver.class,
+                        OracleAddForeignKeyOperationResolver.class,
+                        OracleAddPrimaryKeyOperationResolver.class,
+                        OracleAddUniqueOperationResolver.class,
                         OracleCreateTableOperationResolver.class,
-                        CustomDropTableOperationResolver.class)
+                        OracleDropColumnOperationResolver.class,
+                        OracleDropColumnsOperationResolver.class,
+                        OracleDropForeignKeyOperationResolver.class,
+                        OracleDropPrimaryKeyOperationResolver.class,
+                        OracleDropTableOperationResolver.class,
+                        OracleDropUniqueOperationResolver.class).stream()
                 .map(resolver -> new AbstractMap.SimpleEntry<>(resolver, parserDatabaseProviderFromClass(resolver)))
-                .filter(pair -> pair.getValue() == provider || pair.getValue() == DatabaseProvider.DEFAULT)
+                .filter(pair -> pair.getValue() == provider || pair.getValue() == DatabaseProvider.MULTI)
                 .map(Map.Entry::getKey)
                 .forEach(resolver -> container.register(OperationResolver.class, resolver));
 
@@ -66,7 +90,7 @@ public class SchemaWizardBuilder {
     public SchemaWizardBuilder registerResolver(Class<? extends OperationResolver<?>> resolverClass) {
         DatabaseProvider resolverProvider = parserDatabaseProviderFromClass(resolverClass);
         DatabaseProvider propertiesProvider = parseDatabaseProvider(properties.getConnectionUrl());
-        if (Objects.equals(resolverProvider, propertiesProvider) || Objects.equals(resolverProvider, DatabaseProvider.DEFAULT)) {
+        if (Objects.equals(resolverProvider, propertiesProvider) || Objects.equals(resolverProvider, DatabaseProvider.MULTI)) {
             container.register(OperationResolver.class, resolverClass);
         }
         return this;
@@ -78,20 +102,20 @@ public class SchemaWizardBuilder {
 
     private static DatabaseProvider parseDatabaseProvider(String connectionUrl) {
         if (connectionUrl == null) {
-            return DatabaseProvider.DEFAULT;
+            return DatabaseProvider.MULTI;
         }
         if (connectionUrl.startsWith("jdbc:postgresql")) {
             return DatabaseProvider.POSTGRESQL;
         } else if (connectionUrl.startsWith("jdbc:oracle")) {
             return DatabaseProvider.ORACLE;
         }
-        return DatabaseProvider.DEFAULT;
+        return DatabaseProvider.MULTI;
     }
 
     private static DatabaseProvider parserDatabaseProviderFromClass(Class<? extends OperationResolver<? extends Operation>> resolver) {
         Provider annotation = resolver.getAnnotation(Provider.class);
         if (annotation == null) {
-            return DatabaseProvider.DEFAULT;
+            return DatabaseProvider.MULTI;
         }
         return annotation.value();
     }
